5e12eaa3199e2638ba9bae47ae9624e68e5c7815
Maisam Haider Turi <maisamhaiderturi@gmail.com>
2020-01-07 10:13:56 +0500

    edit task work events issue

--- app/src/main/java/com/example/reminder/Fragments/EditAddReminderDialogFrag.java
+++ app/src/main/java/com/example/reminder/Fragments/EditAddReminderDialogFrag.java
@@ -1,9 +1,11 @@
 package com.example.reminder.Fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 
@@ -14,19 +16,71 @@ import androidx.fragment.app.DialogFragment;
 import com.example.reminder.R;
 
 public class EditAddReminderDialogFrag extends DialogFragment {
-    HorizontalScrollView oneTimeHSV;
-    LinearLayout repeatLL;
+
+    Button oneTimeBtn,repeatBtn,locationSoonBtn;
+    LinearLayout repeatLL,editTagsLL;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate( R.layout.editaddreminderdialogfrag,container,false);
 
-        oneTimeHSV = view.findViewById( R.id.oneTimeHorizontalScrollView );
+        oneTimeBtn = view.findViewById( R.id.oneTime_button );
+        repeatBtn  = view.findViewById( R.id.repeat_button );
+        locationSoonBtn = view.findViewById( R.id.location_button );
+
         repeatLL =view.findViewById( R.id.repeatLL );
+        editTagsLL = view.findViewById( R.id.editTagsLL );
+
+        repeatLL.setVisibility( View.GONE );
+
+        onUpperButtonClick();
 
         return view;
 
+
+    }
+
+
+    public void onUpperButtonClick()
+    {
+        oneTimeBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTagsLL.setVisibility( View.VISIBLE );
+                repeatLL.setVisibility( View.GONE );
+                changeButtonBg(oneTimeBtn,repeatBtn,locationSoonBtn);
+            }
+        } );
+
+        repeatBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeButtonBg(repeatBtn,oneTimeBtn,locationSoonBtn);
+                editTagsLL.setVisibility( View.GONE );
+                repeatLL.setVisibility( View.VISIBLE );
+            }
+        } );
+        locationSoonBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeButtonBg(locationSoonBtn,oneTimeBtn,repeatBtn);
+            }
+        } );
+    }
+    @SuppressLint("ResourceAsColor")
+    public void changeButtonBg(Button setBg, Button removeBg1, Button removeBg2)
+    {
+        setBg.setBackground( getResources().getDrawable( R.drawable.threebuttonsafterclickbg) );
+        setBg.setTextColor( R.color.colorWhite );
+        removeBg1.setBackground( getResources().getDrawable( R.drawable.threebuttonsbeforeclickbg ) );
+        removeBg1.setTextColor( R.color.colorBlack );
+        removeBg2.setBackground( getResources().getDrawable( R.drawable.threebuttonsbeforeclickbg ) );
+        removeBg2.setTextColor( R.color.colorBlack );
+
+    }
+    {
 
     }
 }
